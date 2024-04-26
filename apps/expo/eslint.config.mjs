61888cb2a50c108a80db1a877d67aaeace1fc309
Jaron Heard <jaronheard@gmail.com>
2024-04-25 23:24:40 -0700

    Update package names and imports to use @soonlist instead of @acme

--- apps/expo/eslint.config.mjs
+++ apps/expo/eslint.config.mjs
@@ -1,5 +1,5 @@
-import baseConfig from "@acme/eslint-config/base";
-import reactConfig from "@acme/eslint-config/react";
+import baseConfig from "@soonlist/eslint-config/base";
+import reactConfig from "@soonlist/eslint-config/react";
 
 /** @type {import('typescript-eslint').Config} */
 export default [
--- tooling/prettier/index.js
+++ tooling/prettier/index.js
@@ -21,7 +21,7 @@ const config = {
     "^(expo(.*)$)|^(expo$)",
     "<THIRD_PARTY_MODULES>",
     "",
-    "<TYPES>^@acme",
+    "<TYPES>^@soonlist",
     "^@soonlist/(.*)$",
     "",
     "<TYPES>^[.|..|~]",
