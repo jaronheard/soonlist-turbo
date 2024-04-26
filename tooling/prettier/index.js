404bd2152ed251c1ddbaf785e2b6eae8d106ead9
Jaron Heard <jaronheard@gmail.com>
2024-04-25 21:20:01 -0700

    trailing comma

--- tooling/prettier/index.js
+++ tooling/prettier/index.js
@@ -31,6 +31,7 @@ const config = {
   ],
   importOrderParserPlugins: ["typescript", "jsx", "decorators-legacy"],
   importOrderTypeScriptVersion: "4.4.0",
+  trailingComma: "all",
 };
 
 export default config;
