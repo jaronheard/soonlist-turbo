4e8476a7d1052656c5ed2c7b0a915525e098f771
Jaron Heard <jaronheard@gmail.com>
2024-04-29 10:58:13 -0700

    conditional fix

--- apps/web/components/SampleList.tsx
+++ apps/web/components/SampleList.tsx
@@ -1,6 +1,6 @@
-import { ListCard } from "./ListCard";
 import { EventList } from "~/components/EventList";
 import { api } from "~/trpc/server";
+import { ListCard } from "./ListCard";
 
 export async function SampleList({ listId }: { listId: string }) {
   const list = await api.list.get.query({ listId });
@@ -12,12 +12,13 @@ export async function SampleList({ listId }: { listId: string }) {
   const events = list.eventToLists
     .map((item) => item.event)
     // filter out null events
-    .filter((event) => event.startDateTime)
+    // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+    .filter((event) => event?.startDateTime)
     // sort by startDateTime
     .sort(
       (a, b) =>
         new Date(a.startDateTime).getTime() -
-        new Date(b.startDateTime).getTime()
+        new Date(b.startDateTime).getTime(),
     )
     // filter out past events
     .filter((event) => new Date(event.startDateTime) > new Date())
