dd14b7400762c3ab2123f299ad49f0f7a4a00bee
Jaron Heard <jaronheard@gmail.com>
2024-05-01 13:08:24 -0700

    add warnings for type assertions

--- tooling/eslint/base.js
+++ tooling/eslint/base.js
@@ -76,6 +76,12 @@ export default tseslint.config(
       "@typescript-eslint/no-unsafe-member-access": "warn",
       "@typescript-eslint/require-await": "warn",
       "@typescript-eslint/no-non-null-assertion": "warn",
+      "@typescript-eslint/consistent-type-assertions": [
+        "warn",
+        {
+          assertionStyle: "never",
+        },
+      ],
     },
   },
   {
