8731e599800e288fed27c77de7e6653d3a1d9809
Jaron Heard <jaronheard@gmail.com>
2024-05-02 23:55:22 -0700

    update schema

--- packages/db/src/index.ts
+++ packages/db/src/index.ts
@@ -1,13 +1,10 @@
 import { Client } from "@planetscale/database";
 import { drizzle } from "drizzle-orm/planetscale-serverless";
 
-import * as auth from "./schema/auth";
-import * as post from "./schema/post";
+import * as schema from "./schema";
 
 export * from "drizzle-orm/sql";
 export { alias } from "drizzle-orm/mysql-core";
-
-export const schema = { ...auth, ...post };
 
 const psClient = new Client({ url: process.env.DATABASE_URL || "" });
 export const db = drizzle(psClient, { schema });
