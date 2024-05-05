77777bbb2645f6fa7bfc1e01f3537772ecce335a
Jaron Heard <jaronheard@gmail.com>
2024-05-05 16:35:35 -0700

    load lists on /new page

--- apps/expo/src/app/events/index.tsx
+++ apps/expo/src/app/events/index.tsx
@@ -27,24 +27,14 @@ export default function Index() {
   return (
     <SafeAreaView className=" bg-background">
       {/* Changes page title visible on the header */}
-      <Stack.Screen options={{ title: "Home Page" }} />
+      <Stack.Screen options={{ title: "All Events" }} />
       <View className="h-full w-full bg-background p-4">
-        <Text className="pb-2 text-center text-5xl font-bold text-foreground">
-          Create <Text className="text-primary">T3</Text> Turbo
-        </Text>
-
         <Pressable
           onPress={() => void utils.event.invalidate()}
           className="flex items-center rounded-lg bg-primary p-2"
         >
           <Text className="text-foreground"> Refresh events</Text>
         </Pressable>
-
-        <View className="py-2">
-          <Text className="font-semibold italic text-primary">
-            Press on a event
-          </Text>
-        </View>
 
         {eventsQuery.data && <UserEventsList events={eventsQuery.data} />}
 
