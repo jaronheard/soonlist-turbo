c1463ac2902ca92ea1ed1cbde0ae2c496ec7f03a
Jaron Heard <jaronheard@gmail.com>
2024-05-15 14:53:31 -0700

    remove sentry from app so i can re-add with wizard

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,5 +1,3 @@
-import { withSentryConfig } from "@sentry/nextjs";
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   /** T3 defaults */
@@ -75,43 +73,4 @@ const nextConfig = {
   },
 };
 
-// Injected content via Sentry wizard below
-const nextConfigWithSentry =
-  process.env.NODE_ENV === "production"
-    ? withSentryConfig(
-        nextConfig,
-        {
-          // For all available options, see:
-          // https://github.com/getsentry/sentry-webpack-plugin#options
-
-          // Suppresses source map uploading logs during build
-          silent: true,
-          org: "soonlist",
-          project: "soonlist",
-        },
-        {
-          // For all available options, see:
-          // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
-
-          // Upload a larger set of source maps for prettier stack traces (increases build time)
-          widenClientFileUpload: true,
-
-          // Transpiles SDK to be compatible with IE11 (increases bundle size)
-          transpileClientSDK: false,
-
-          // Hides source maps from generated client bundles
-          hideSourceMaps: true,
-
-          // Automatically tree-shake Sentry logger statements to reduce bundle size
-          disableLogger: true,
-
-          // Enables automatic instrumentation of Vercel Cron Monitors.
-          // See the following for more information:
-          // https://docs.sentry.io/product/crons/
-          // https://vercel.com/docs/cron-jobs
-          automaticVercelMonitors: true,
-        },
-      )
-    : nextConfig;
-
-export default nextConfigWithSentry;
+export default nextConfig;
