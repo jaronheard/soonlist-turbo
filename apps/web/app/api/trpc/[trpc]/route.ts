0279055110627615f55f54640bc392f6d4560e6b
Jaron Heard <jaronheard@gmail.com>
2024-05-15 22:43:19 -0700

    try new trpc logging

--- apps/web/app/api/trpc/[trpc]/route.ts
+++ apps/web/app/api/trpc/[trpc]/route.ts
@@ -1,6 +1,8 @@
 import type { NextRequest } from "next/server";
 import * as Sentry from "@sentry/nextjs";
+import { TRPCError } from "@trpc/server";
 import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
+import { getHTTPStatusCodeFromError } from "@trpc/server/http";
 
 import { appRouter } from "@soonlist/api/root";
 import { createTRPCContext } from "@soonlist/api/trpc";
@@ -23,15 +25,27 @@ const handler = (req: NextRequest) =>
     req,
     router: appRouter,
     createContext: () => createContext(req),
-    onError: ({ path, error }) => {
-      try {
-        throw new Error(
-          `❌ tRPC failed on ${path ?? "<no-path>"}: ${error.message}`,
-        );
-      } catch (err) {
-        Sentry.captureException(err);
-        // Optional: You can log the error or perform any other necessary actions
-        console.error(err);
+    onError: ({ path, error, ctx, input, req, type }) => {
+      const statusCode = getHTTPStatusCodeFromError(error);
+      if (statusCode >= 500) {
+        try {
+          console.error(error, error.stack);
+          const mainError =
+            error instanceof TRPCError ? error.cause || error : error;
+          throw mainError;
+        } catch (err) {
+          Sentry.captureException(err, {
+            extra: {
+              statusCode,
+              error,
+              type,
+              path,
+              input,
+              ctx,
+              req,
+            },
+          });
+        }
       }
     },
   });
