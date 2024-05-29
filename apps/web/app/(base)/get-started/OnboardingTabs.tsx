f508ff4f01ebcc446eb0386d9018c7f7d71fb7b0
Jaron Heard <jaronheard@gmail.com>
2024-05-28 18:46:04 -0700

    remove irrelevant parts of onboarding

--- apps/web/app/(base)/get-started/OnboardingTabs.tsx
+++ apps/web/app/(base)/get-started/OnboardingTabs.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import type { z } from "zod";
-import { useState } from "react";
 import { useRouter } from "next/navigation";
 import { zodResolver } from "@hookform/resolvers/zod";
 import {
@@ -34,7 +33,6 @@ import {
   FormMessage,
 } from "@soonlist/ui/form";
 import { Input } from "@soonlist/ui/input";
-import { Tabs, TabsContent, TabsList, TabsTrigger } from "@soonlist/ui/tabs";
 import { Textarea } from "@soonlist/ui/textarea";
 import { userAdditionalInfoSchema } from "@soonlist/validators";
 
@@ -46,115 +44,23 @@ export function OnboardingTabs({
 }: {
   additionalInfo: z.infer<typeof userAdditionalInfoSchema>;
 }) {
-  const [activeTab, setActiveTab] = useState("profile");
-
-  const handleTabChange = (value: string) => {
-    setActiveTab(value);
-  };
+  const router = useRouter();
 
   return (
-    <Tabs
-      className="w-full max-w-3xl"
-      defaultValue="profile"
-      value={activeTab}
-      onValueChange={handleTabChange}
-    >
-      <TabsList className="grid grid-cols-2">
-        <TabsTrigger className="relative" value="profile">
-          <ProgressIcon status="complete"></ProgressIcon>
-          <span className="block text-sm font-medium">Profile</span>
-        </TabsTrigger>
-        <TabsTrigger className="relative" value="setup">
-          <ProgressIcon status="active"></ProgressIcon>
-          <span className="block text-sm font-medium">Get Started</span>
-        </TabsTrigger>
-      </TabsList>
-      <TabsContent value="profile">
-        <Card className="w-full max-w-3xl">
-          <CardHeader className="space-y-1">
-            <CardTitle>Profile</CardTitle>
-            <CardDescription>
-              Tell people about yourself and how to connect!
-            </CardDescription>
-          </CardHeader>
-          <CardContent className="space-y-6">
-            <UserProfileForm
-              defaultValues={additionalInfo}
-              onSubmitSuccess={() => {
-                scrollTo(0, 0);
-                handleTabChange("setup");
-              }}
-            />
-          </CardContent>
-        </Card>
-      </TabsContent>
-      <TabsContent value="setup">
-        <Card className="w-full max-w-3xl">
-          <CardHeader className="space-y-1">
-            <CardTitle>Get Started</CardTitle>
-            <CardDescription>
-              Install the shortcut and start creating events!
-            </CardDescription>
-          </CardHeader>
-          <CardContent className="prose space-y-6">
-            <ol>
-              <li>
-                <p className="font-bold">Ensure your device is up to date</p>
-                <p>
-                  To use the iOS/MacOS Shortcut, you must have the latest major
-                  version of{" "}
-                  <a
-                    href="https://support.apple.com/en-us/HT201685"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    iOS (&gt;17)
-                  </a>{" "}
-                  or{" "}
-                  <a
-                    href="https://support.apple.com/en-us/109033"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    MacOS (&gt;14)
-                  </a>{" "}
-                  installed.
-                </p>
-              </li>
-              <li>
-                <p className="font-bold">Install the iOS/MacOS Shortcut</p>
-                <Button asChild size={"sm"} variant={"secondary"}>
-                  <a
-                    href="https://www.icloud.com/shortcuts/a44e63d78fd44a08b22dcaaea2bfa7f6"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                    className="no-underline"
-                  >
-                    Download shortcut
-                  </a>
-                </Button>
-              </li>
-              <li>
-                <p className="font-bold">Test the iOS/MacOS Shortcut</p>
-                <Button size="sm" asChild>
-                  <a
-                    href="shortcuts://run-shortcut?name=Add%20to%20Soonlist&input=Test%20event%20that%20proves%20your%20shortcut%20is%20working%20at%20the%20School%20of%20Art%20and%20Time%203340%20SE%20Morrison%20on%20Saturday%20February%2017%20from%206pm%20to%209pm%20Text%20Jaron%20at%209719987180%20for%20access%20instructions"
-                    className="no-underline"
-                  >
-                    Test shortcut
-                  </a>
-                </Button>
-                <p>This will create your first Soonlist event!</p>
-              </li>
-            </ol>
-            <p>
-              If you are encountering any issues, please send us a message via
-              the chat widget.
-            </p>
-          </CardContent>
-        </Card>
-      </TabsContent>
-    </Tabs>
+    <Card className="mx-auto w-full max-w-3xl">
+      <CardHeader className="space-y-1">
+        <CardTitle>Public Profile</CardTitle>
+        <CardDescription>
+          This information will be shown on events you create.
+        </CardDescription>
+      </CardHeader>
+      <CardContent className="space-y-6">
+        <UserProfileForm
+          defaultValues={additionalInfo}
+          onSubmitSuccess={() => router.push("/new")}
+        />
+      </CardContent>
+    </Card>
   );
 }
 
@@ -216,8 +122,7 @@ export function UserProfileForm({
           <CardHeader>
             <CardTitle>How to connect</CardTitle>
             <CardDescription>
-              Share any contact info you&apos;d like visible publicly so others
-              can connect with you.
+              Share any contact info you want visible publicly.
             </CardDescription>
           </CardHeader>
           <CardContent>
@@ -289,7 +194,7 @@ export function UserProfileForm({
             </div>
           </CardContent>
         </Card>
-        <Button type="submit">Next</Button>
+        <Button type="submit">Add Your First Event</Button>
       </form>
     </Form>
   );
