7cd6a35d619d3ea80b943ee9fb0f21311210c412
Jaron Heard <jaronheard@gmail.com>
2024-06-05 16:59:32 -0700

    adjust to get all subscription urls

--- packages/api/src/routers/stripe.ts
+++ packages/api/src/routers/stripe.ts
@@ -1,58 +1,59 @@
 import { TRPCError } from "@trpc/server";
 import Stripe from "stripe";
-import { z } from "zod";
 
 import { createTRPCRouter, protectedProcedure } from "../trpc";
 
 export const stripeRouter = createTRPCRouter({
-  getSubscriptionCheckoutURL: protectedProcedure
-    .input(z.object({ plan: z.enum(["personal", "pro"]) }))
-    .query(async ({ ctx, input }) => {
-      const plans = {
-        personal: {
-          name: "Personal",
-          priceId: process.env.STRIPE_PRICE_ID_PERSONAL_MONTHLY || "",
-        },
-        pro: {
-          name: "Pro",
-          priceId: process.env.STRIPE_PRICE_ID_PRO_MONTHLY || "",
-        },
-      };
+  getSubscriptionCheckoutUrls: protectedProcedure.query(async ({ ctx }) => {
+    const plans = {
+      personal: {
+        name: "Personal",
+        priceId: process.env.STRIPE_PRICE_ID_PERSONAL_MONTHLY || "",
+      },
+      pro: {
+        name: "Pro",
+        priceId: process.env.STRIPE_PRICE_ID_PRO_MONTHLY || "",
+      },
+    };
 
-      const planPriceId = plans[input.plan].priceId;
+    const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || "", {
+      apiVersion: "2024-04-10",
+    });
 
-      const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || "", {
-        apiVersion: "2024-04-10",
-      });
+    const url = process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL;
 
-      const url = process.env.NEXT_PUBLIC_VERCEL_PROJECT_PRODUCTION_URL;
-
-      const checkoutSession = await stripe.checkout.sessions.create({
-        mode: "subscription",
-        line_items: [
-          {
-            price: planPriceId,
-            quantity: 1,
+    const checkoutUrls = await Promise.all(
+      Object.keys(plans).map(async (planKey) => {
+        const plan = plans[planKey as keyof typeof plans];
+        const checkoutSession = await stripe.checkout.sessions.create({
+          mode: "subscription",
+          line_items: [
+            {
+              price: plan.priceId,
+              quantity: 1,
+            },
+          ],
+          success_url: `${url}/get-started`,
+          cancel_url: `${url}/account/plans`,
+          subscription_data: {
+            metadata: {
+              userId: ctx.auth.userId,
+            },
+            trial_period_days: 14,
           },
-        ],
-
-        success_url: `${url}/get-started`,
-        cancel_url: `${url}/account/plans`,
-        subscription_data: {
-          metadata: {
-            userId: ctx.auth.userId,
-          },
-          trial_period_days: 14,
-        },
-      });
-
-      if (!checkoutSession.url) {
-        throw new TRPCError({
-          code: "BAD_REQUEST",
-          message: "Could not create checkout message",
         });
-      }
 
-      return { redirectURL: checkoutSession.url };
-    }),
+        if (!checkoutSession.url) {
+          throw new TRPCError({
+            code: "BAD_REQUEST",
+            message: `Could not create checkout session for ${plan.name} plan`,
+          });
+        }
+
+        return { plan: plan.name, redirectURL: checkoutSession.url };
+      }),
+    );
+
+    return checkoutUrls;
+  }),
 });
