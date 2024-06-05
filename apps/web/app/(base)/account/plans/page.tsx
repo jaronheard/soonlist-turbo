a5416469d13345a3f2e18e808ba0af4c323da829
Jaron Heard <jaronheard@gmail.com>
2024-06-05 15:50:24 -0700

    add plans page

---
+++ apps/web/app/(base)/account/plans/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import { Pricing } from "~/app/(marketing)/components";
+
+export const metadata = {
+  title: "Plans | Soonlist",
+  openGraph: {
+    title: "Plans | Soonlist",
+  },
+};
+
+export default function Page() {
+  return <Pricing />;
+}
