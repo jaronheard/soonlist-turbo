5cd12b5ba4c5227e5322341038ac938dc3e414bc
Jaron Heard <jaronheard@gmail.com>
2024-06-07 10:54:09 -0700

    about page update, remove onboarding page

--- apps/web/app/(base)/about/page.tsx
+++ apps/web/app/(base)/about/page.tsx
@@ -1,12 +1,6 @@
 import React from "react";
-import Link from "next/link";
-import { Currency } from "lucide-react";
-
-import { Button } from "@soonlist/ui/button";
 
 import { CTAButton } from "~/components/CallToActions";
-import { ContactUs } from "~/components/ContactUs";
-import { RainbowText } from "~/components/RainbowText";
 
 export const metadata = {
   title: "About | Soonlist",
@@ -17,115 +11,63 @@ export const metadata = {
 
 export default function Page() {
   return (
-    <div className="bg-white px-6 py-32 lg:px-8">
-      <div className="mx-auto max-w-3xl text-base leading-7 text-gray-700">
-        <RainbowText className="text-base font-semibold leading-7">
-          About
-        </RainbowText>
-        <h1 className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-          Soonlist
-        </h1>
-        <h2 className="mt-16 text-2xl font-bold tracking-tight text-gray-900">
-          Our Vision
-        </h2>
-        <p className="mt-6 max-w-xl text-xl leading-8">
-          To allow individuals and organizations to easily curate, collect and
-          share events in a way that allows for more cross-pollination,
-          community, and coalition building, through a transparent,
-          community-owned platform … and seamlessly integrate with our digital
-          calendars.
+    <div>
+      <div className="prose mx-auto sm:prose-lg lg:prose-xl xl:prose-2xl 2xl:prose-2xl">
+        <h1 className="font-heading">About Soonlist</h1>
+        <h2 className="font-heading">Our vision</h2>
+        <p className="">
+          We want to connect people more to what they care about, and reclaim
+          our collective attention. Soonlist aims to make saving, organizing,
+          and sharing events as easy and fun as making playlists or sharing a
+          photo album.
         </p>
-        <div className="bg-white pt-16">
-          <div className="mx-auto max-w-xl text-base leading-7 text-gray-700">
-            <RainbowText className="text-base font-semibold leading-7">
-              Transparency
-            </RainbowText>
-            <h2 className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
-              Building in Public
-            </h2>
-            <p className="mt-6 text-xl leading-8">
-              We believe in the power of transparency and community engagement.
-              That&apos;s why we&apos;re building in public, open-source,
-              towards community ownership.
-            </p>
-            <h3 className="mt-12 text-2xl font-bold tracking-tight text-gray-900">
-              Business Model
-            </h3>
-            <p className="mt-6">
-              We are exploring sources of revenue that support our goal of
-              community ownership. Have an idea?{" "}
-              <ContactUs>Let us know!</ContactUs> Our three best ideas:
-            </p>
-            <ul role="list" className="mt-8 max-w-xl space-y-8 text-gray-600">
-              <li className="flex gap-x-3">
-                <Currency
-                  className="mt-1 size-5 flex-none text-gray-600"
-                  aria-hidden="true"
-                />
-                <span>
-                  <strong className="font-semibold text-gray-900">
-                    Members Support Curators:
-                  </strong>{" "}
-                  Soonlist users can financially support event curators through
-                  tips/subscriptions, and Soonlist takes a cut to support
-                  development.
-                </span>
-              </li>
-              <li className="flex gap-x-3">
-                <Currency
-                  className="mt-1 size-5 flex-none text-gray-600"
-                  aria-hidden="true"
-                />
-                <span>
-                  <strong className="font-semibold text-gray-900">
-                    Free/Supporter Monthly Subscriptions:
-                  </strong>{" "}
-                  Some features are just available to paying members. We want
-                  most members to be able to use Soonlist for free, so this
-                  would be more advanced features or customizations.
-                </span>
-              </li>
-              <li className="flex gap-x-3">
-                <Currency
-                  className="mt-1 size-5 flex-none text-gray-600"
-                  aria-hidden="true"
-                />
-                <span>
-                  <strong className="font-semibold text-gray-900">
-                    Commercial Events:
-                  </strong>{" "}
-                  We could find ways to monetize larger, commercial events to
-                  make Soonlist free for smaller or non-commercial events.
-                </span>
-              </li>
-            </ul>
-            <h3 className="mt-12 text-2xl font-bold tracking-tight text-gray-900">
-              Collaborative Open Source
-            </h3>
-            <p className="mt-6">
-              Soonlist is more than a platform; it&apos;s a collaborative
-              endeavor. We&apos;re open-source, meaning{" "}
-              <a
-                href="https://www.icloud.com/shortcuts/a44e63d78fd44a08b22dcaaea2bfa7f6"
-                className="text-blue-500 underline"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                our codebase is publicly available
-              </a>{" "}
-              , allowing anyone to contribute, inspect, or adapt it.
-            </p>
-          </div>
+        <p>We imagine a world where:</p>
+        <ul>
+          <li>People are better connected to what they care about 💖</li>
+          <li>Social media isn't required to know what's going on 🙅‍♀️</li>
+          <li>
+            People are saving, sharing, and attending events that inspire them
+            🌟
+          </li>
+          <li>
+            Anyone can create curated lists of events they're interested in 🎨
+          </li>
+          <li>
+            Communities grow because it’s easy to see what events are happening
+            🙌
+          </li>
+          <li>
+            There's more cross-pollination across circles, communities, and
+            cultures 🌍
+          </li>
+          <li>We have more time to be together 👥</li>
+        </ul>
+        <div className="">
+          <h2 className="font-heading">Our story</h2>
+          <p className="">
+            Soonlist was founded by{" "}
+            <a href="https://www.jaronheard.com">Jaron Heard</a> in October 2023
+            inspired by a coalescing of community needs, ideas, and technology
+            developments. Community organizations like{" "}
+            <a href="https://www.makingearthcool.com">Making Earth Cool</a> were
+            facing the reality of how difficult it was to save and gather
+            events. Some communities made their own calendars, but it required
+            lots of manual work or special skills. At the same time, new
+            advances in technology made it possible to extract event information
+            from text, images, and websites more effectively than ever before.
+          </p>
+          <p>
+            Since then, Jaron and friends have been exploring we can use this
+            new technology to. Soonlist is both already a useful product … and
+            it’s still super early in its development! We’re in the process of
+            looking for a co-founder/builder.{" "}
+            <a href="mailto:jaron@soonlist.com">Email us</a> if you have a
+            connection!
+          </p>
         </div>
-        <div className="p-6"></div>
-        <div className="flex items-center gap-x-6 lg:shrink-0">
-          <CTAButton />
-          <Button variant="link" asChild>
-            <Link href="/explore">
-              Discover <span aria-hidden="true">→</span>
-            </Link>
-          </Button>
-        </div>
+      </div>
+      <div className="mx-auto flex max-w-min gap-6">
+        <CTAButton />
       </div>
     </div>
   );
