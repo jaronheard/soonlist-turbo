03787babc5eeab4cb8c6ce7a71a50aab30d27e0c
Jaron Heard <jaronheard@gmail.com>
2024-06-09 21:44:35 -0700

    remove edge

--- apps/web/app/layout.tsx
+++ apps/web/app/layout.tsx
@@ -8,9 +8,6 @@ import { IBM_Plex_Sans, Kalam } from "next/font/google";
 import { TRPCReactProvider } from "~/trpc/react";
 import { PHProvider, Providers } from "./providers";
 
-// edge causes sigkill on vercel about 50% of the time
-// workaround is to use VERCEL_FORCE_NO_BUILD_CACHE=1
-export const runtime = "edge";
 export const preferredRegion = "pdx1";
 
 const kalam = Kalam({
