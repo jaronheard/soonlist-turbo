16d7dab6d44c7d6c50d7787bad1460fedd5f9ad3
Jaron Heard <jaronheard@gmail.com>
2024-06-24 21:31:12 -0700

    unmatched

---
+++ apps/expo/src/app/[...unmatched].tsx
@@ -0,0 +1,5 @@
+// for iOS deeplink redirect to this unmatched page
+// catched link: exposhareintentexample:///dataUrl=exposhareintentexampleShareKey
+import Home from "./index";
+
+export default Home;
