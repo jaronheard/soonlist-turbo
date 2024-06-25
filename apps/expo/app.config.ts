85c9835ae3dfe53e520d73df870b23ff5b3ef4fa
Jaron Heard <jaronheard@gmail.com>
2024-06-24 20:14:59 -0700

    skip a step when submitting to app store

--- apps/expo/app.config.ts
+++ apps/expo/app.config.ts
@@ -49,6 +49,9 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   ios: {
     supportsTablet: true,
     bundleIdentifier: "soonlist.soonlist",
+    config: {
+      usesNonExemptEncryption: false,
+    },
   },
   android: {
     package: "soonlist.soonlist",
