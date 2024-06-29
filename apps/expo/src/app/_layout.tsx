01627c42c033bc64c443f87ad66c747eacb2c210
Jaron Heard <jaronheard@gmail.com>
2024-06-28 17:01:25 -0700

    jaronheard/tim 298 setup push notifications
    * push notification basic setup
    
    * update push notification message
    
    * remove console logs

--- apps/expo/src/app/_layout.tsx
+++ apps/expo/src/app/_layout.tsx
@@ -1,15 +1,17 @@
+import { useEffect, useRef, useState } from "react";
+import { Button, Platform, Text, View } from "react-native";
+import Constants from "expo-constants";
+import * as Device from "expo-device";
+import * as Notifications from "expo-notifications";
 import { Stack } from "expo-router";
 import * as SecureStore from "expo-secure-store";
 import { StatusBar } from "expo-status-bar";
 import { ClerkProvider } from "@clerk/clerk-expo";
+import { useColorScheme } from "nativewind";
 
 import { TRPCProvider } from "~/utils/api";
 
 import "../styles.css";
-
-import { Text } from "react-native";
-import Constants from "expo-constants";
-import { useColorScheme } from "nativewind";
 
 const tokenCache = {
   async getToken(key: string) {
@@ -28,9 +30,125 @@ const tokenCache = {
   },
 };
 
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    shouldShowAlert: true,
+    shouldPlaySound: false,
+    shouldSetBadge: false,
+  }),
+});
+
+export async function sendPushNotification(expoPushToken: string) {
+  const message = {
+    to: expoPushToken,
+    sound: "default",
+    title: "Want in early?",
+    body: "You can be one of the first. We want to learn together.",
+    data: { someData: "goes here" },
+  };
+
+  await fetch("https://exp.host/--/api/v2/push/send", {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Accept-encoding": "gzip, deflate",
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(message),
+  });
+}
+
+function handleRegistrationError(errorMessage: string) {
+  console.error(errorMessage);
+  throw new Error(errorMessage);
+}
+
+async function registerForPushNotificationsAsync() {
+  if (Platform.OS === "android") {
+    void Notifications.setNotificationChannelAsync("default", {
+      name: "default",
+      importance: Notifications.AndroidImportance.MAX,
+      vibrationPattern: [0, 250, 250, 250],
+      lightColor: "#FF231F7C",
+    });
+  }
+
+  if (Device.isDevice) {
+    const { status: existingStatus } =
+      await Notifications.getPermissionsAsync();
+    let finalStatus = existingStatus;
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-enum-comparison
+    if (existingStatus !== "granted") {
+      const { status } = await Notifications.requestPermissionsAsync();
+      finalStatus = status;
+    }
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-enum-comparison
+    if (finalStatus !== "granted") {
+      handleRegistrationError(
+        "Permission not granted to get push token for push notification!",
+      );
+      return;
+    }
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+    const projectId: string | undefined =
+      // eslint-disable-next-line @typescript-eslint/no-unsafe-member-access
+      Constants.expoConfig?.extra?.eas?.projectId ??
+      Constants.easConfig?.projectId;
+    if (!projectId) {
+      handleRegistrationError("Project ID not found");
+    }
+    try {
+      const pushTokenString = (
+        await Notifications.getExpoPushTokenAsync({
+          projectId,
+        })
+      ).data;
+      console.log(pushTokenString);
+      return pushTokenString;
+    } catch (e: unknown) {
+      // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+      handleRegistrationError(`${e}`);
+    }
+  } else {
+    handleRegistrationError("Must use physical device for push notifications");
+  }
+}
+
 // This is the main layout of the app
 // It wraps your pages with the providers they need
 export default function RootLayout() {
+  const [expoPushToken, setExpoPushToken] = useState("");
+  const [notification, setNotification] = useState<
+    Notifications.Notification | undefined
+  >(undefined);
+  const notificationListener = useRef<Notifications.Subscription>();
+  const responseListener = useRef<Notifications.Subscription>();
+
+  useEffect(() => {
+    registerForPushNotificationsAsync()
+      .then((token) => setExpoPushToken(token ?? ""))
+      .catch((error: string) => setExpoPushToken(`${error}`));
+
+    notificationListener.current =
+      Notifications.addNotificationReceivedListener((notification) => {
+        setNotification(notification);
+      });
+
+    responseListener.current =
+      Notifications.addNotificationResponseReceivedListener((response) => {
+        console.log(response);
+      });
+
+    return () => {
+      notificationListener.current &&
+        Notifications.removeNotificationSubscription(
+          notificationListener.current,
+        );
+      responseListener.current &&
+        Notifications.removeNotificationSubscription(responseListener.current);
+    };
+  }, []);
+
   const { colorScheme } = useColorScheme();
   // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
   const clerkPublishableKey = Constants.expoConfig?.extra
@@ -60,6 +178,30 @@ export default function RootLayout() {
             },
           }}
         />
+        <View
+          style={{
+            flex: 1,
+            alignItems: "center",
+            justifyContent: "space-around",
+          }}
+        >
+          <Text>Your Expo push token: {expoPushToken}</Text>
+          <View style={{ alignItems: "center", justifyContent: "center" }}>
+            <Text>Title: {notification?.request.content.title} </Text>
+            <Text>Body: {notification?.request.content.body}</Text>
+            <Text>
+              Data:{" "}
+              {notification &&
+                JSON.stringify(notification.request.content.data)}
+            </Text>
+          </View>
+          <Button
+            title="Press to Send Notification"
+            onPress={async () => {
+              await sendPushNotification(expoPushToken);
+            }}
+          />
+        </View>
         <StatusBar />
       </ClerkProvider>
     </TRPCProvider>
