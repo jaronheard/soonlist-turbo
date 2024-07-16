1031050f990471c1c6470b89dacf19c32406aab5
Jaron Heard <jaronheard@gmail.com>
2024-07-15 22:03:15 -0700

    add eventFromImageThenCreateThenNotification

--- packages/api/src/routers/ai.ts
+++ packages/api/src/routers/ai.ts
@@ -46,6 +46,17 @@ const prototypeEventCreateSchema = z.object({
   userId: z.string(),
   username: z.string(),
 });
+
+const prototypeEventCreateFromImageSchema = z.object({
+  imageUrl: z.string(),
+  timezone: z.string(),
+  expoPushToken: z.string(),
+  comment: z.string().optional(),
+  lists: z.array(z.record(z.string().trim())),
+  visibility: z.enum(["public", "private"]).optional(),
+  userId: z.string(),
+  username: z.string(),
+});
 // Create a single Expo SDK client to be reused
 const expo = new Expo();
 
@@ -556,6 +567,323 @@ export const aiRouter = createTRPCRouter({
         const eventObject = { ...event.object, eventMetadata: metadata.object };
 
         const events = addCommonAddToCalendarProps([eventObject]);
+        // const response = `${event.rawResponse?.toString() || ""} ${metadata.rawResponse?.toString()}`;
+        // return { events, response };
+
+        // adapted logic from event.create
+        const userId = input.userId;
+        const username = input.username;
+        if (!userId) {
+          throw new TRPCError({
+            code: "INTERNAL_SERVER_ERROR",
+            message: "No user id found in session",
+          });
+        }
+
+        if (!username) {
+          throw new TRPCError({
+            code: "INTERNAL_SERVER_ERROR",
+            message: "No username found in session",
+          });
+        }
+
+        const firstEvent = events[0];
+        if (!firstEvent) {
+          throw new TRPCError({
+            code: "INTERNAL_SERVER_ERROR",
+            message: "No events found in response",
+          });
+        }
+
+        const hasComment = input.comment && input.comment.length > 0;
+        const hasLists = input.lists.length > 0;
+        const hasVisibility = input.visibility && input.visibility.length > 0;
+
+        let startTime = firstEvent.startTime;
+        let endTime = firstEvent.endTime;
+        let timeZone = firstEvent.timeZone;
+
+        // time zone is America/Los_Angeles if not specified
+        if (!timeZone) {
+          timeZone = "America/Los_Angeles";
+        }
+
+        // start time is 00:00 if not specified
+        if (!startTime) {
+          startTime = "00:00";
+        }
+        // end time is 23:59 if not specified
+        if (!endTime) {
+          endTime = "23:59";
+        }
+
+        const start = Temporal.ZonedDateTime.from(
+          `${firstEvent.startDate}T${startTime}[${timeZone}]`,
+        );
+        const end = Temporal.ZonedDateTime.from(
+          `${firstEvent.endDate}T${endTime}[${timeZone}]`,
+        );
+        const startUtcDate = new Date(start.epochMilliseconds);
+        const endUtcDate = new Date(end.epochMilliseconds);
+        const eventid = generatePublicId();
+
+        const values = {
+          id: eventid,
+          userId: userId,
+          userName: username || "unknown",
+          event: firstEvent,
+          eventMetadata: firstEvent.eventMetadata,
+          startDateTime: startUtcDate,
+          endDateTime: endUtcDate,
+          ...(hasVisibility && {
+            visibility: input.visibility,
+          }),
+        };
+        const createEvent = await ctx.db
+          .transaction(async (tx) => {
+            const insertEvent = async (event: NewEvent) => {
+              return tx.insert(eventsSchema).values(event);
+            };
+            const insertComment = async (comment: NewComment) => {
+              return tx.insert(comments).values(comment);
+            };
+            const insertEventToLists = async (
+              eventToList: NewEventToLists[],
+            ) => {
+              return tx.insert(eventToLists).values(eventToList);
+            };
+
+            await insertEvent(values);
+            if (hasComment) {
+              await insertComment({
+                eventId: eventid,
+                content: input.comment || "",
+                userId: userId,
+              });
+            } else {
+              // no need to insert comment if there is no comment
+            }
+            if (hasLists) {
+              await tx
+                .delete(eventToLists)
+                .where(eq(eventToLists.eventId, eventid));
+              await insertEventToLists(
+                input.lists.map((list) => ({
+                  eventId: eventid,
+                  listId: list.value!,
+                })),
+              );
+            } else {
+              // no need to insert event to list if there is no list
+            }
+          })
+          .then(() => ({ id: eventid }));
+        console.log("createEvent", createEvent);
+        const { expoPushToken } = input;
+
+        const title = "Soonlist";
+        const body = "Your event is ready to go!";
+        const data = { url: `/event/${createEvent.id}` };
+
+        if (!Expo.isExpoPushToken(expoPushToken)) {
+          throw new Error(
+            // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+            `Push token ${expoPushToken} is not a valid Expo push token`,
+          );
+        }
+
+        const message: ExpoPushMessage = {
+          to: expoPushToken,
+          sound: "default",
+          title,
+          body,
+          data,
+        };
+
+        try {
+          const [ticket] = await expo.sendPushNotificationsAsync([message]);
+          return {
+            success: true,
+            ticket,
+          };
+        } catch (error) {
+          console.error("Error sending notification:", error);
+          return {
+            success: false,
+            // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
+            error: (error as Error).message,
+          };
+        }
+      } catch (error) {
+        const { expoPushToken } = input;
+
+        const title = "Soonlist";
+        const body = "There was an error creating your event.";
+        const data = { url: "/new/preview" };
+
+        if (!Expo.isExpoPushToken(expoPushToken)) {
+          throw new Error(
+            // eslint-disable-next-line @typescript-eslint/restrict-template-expressions
+            `Push token ${expoPushToken} is not a valid Expo push token`,
+          );
+        }
+
+        const message: ExpoPushMessage = {
+          to: expoPushToken,
+          sound: "default",
+          title,
+          body,
+          data,
+        };
+
+        try {
+          const [ticket] = await expo.sendPushNotificationsAsync([message]);
+          return {
+            success: true,
+            ticket,
+          };
+        } catch (error) {
+          console.error("Error sending notification:", error);
+          return {
+            success: false,
+            // eslint-disable-next-line @typescript-eslint/consistent-type-assertions
+            error: (error as Error).message,
+          };
+        }
+      }
+    }),
+  eventFromImageThenCreateThenNotification: publicProcedure
+    .input(prototypeEventCreateFromImageSchema)
+    .mutation(async ({ ctx, input }) => {
+      const system = getSystemMessage();
+      const systemMetadata = getSystemMessageMetadata();
+      const prompt = getPrompt(input.timezone);
+
+      console.log("input", SuperJSON.stringify(input));
+
+      // Reuse the generateObjectWithLogging function
+      const generateObjectWithLogging = async <T>(
+        generateObjectOptions: Parameters<typeof generateObject<T>>[0],
+        loggingOptions: { name: string },
+      ): Promise<ReturnType<typeof generateObject<T>>> => {
+        const trace = langfuse.trace({
+          name: loggingOptions.name,
+          sessionId: ctx.auth.sessionId,
+          userId: ctx.auth.userId,
+          input: input.imageUrl,
+          version: prompt.version,
+        });
+        const generation = trace.generation({
+          name: "generation",
+          input: input.imageUrl,
+          model: MODEL,
+          version: prompt.version,
+        });
+        generation.update({
+          completionStartTime: new Date(),
+        });
+        try {
+          const result = await generateObject(generateObjectOptions);
+          generation.end({
+            output: result.object,
+          });
+          generation.score({
+            name: "eventToJson",
+            value: result.object === null ? 0 : 1,
+            comment: "Untested",
+          });
+          trace.update({
+            output: result.object,
+            metadata: {
+              finishReason: result.finishReason,
+              logprobs: result.logprobs,
+              rawResponse: result.rawResponse,
+              warnings: result.warnings,
+            },
+          });
+          waitUntil(langfuse.flushAsync());
+          return result;
+        } catch (error) {
+          console.error(
+            "An error occurred while generating the response:",
+            error,
+          );
+          generation.score({
+            name: "eventToJson",
+            value: 0,
+          });
+          trace.update({
+            output: null,
+            metadata: {
+              finishReason: "error",
+              error: error,
+            },
+          });
+          waitUntil(langfuse.flushAsync());
+          throw error;
+        }
+      };
+
+      try {
+        const [event, metadata] = await Promise.all([
+          generateObjectWithLogging(
+            {
+              model: openai(MODEL),
+              mode: "json",
+              temperature: 0.2,
+              maxRetries: 0,
+              messages: [
+                { role: "system", content: system.text },
+                {
+                  role: "user",
+                  content: prompt.text,
+                },
+                {
+                  role: "user",
+                  content: [
+                    {
+                      type: "image",
+                      image: new URL(input.imageUrl),
+                    },
+                  ],
+                },
+              ],
+              schema: EventSchema,
+            },
+            { name: "eventFromImage.event" },
+          ),
+          generateObjectWithLogging(
+            {
+              model: openai(MODEL),
+              mode: "json",
+              temperature: 0.2,
+              maxRetries: 0,
+              messages: [
+                { role: "system", content: systemMetadata.text },
+                {
+                  role: "user",
+                  content: prompt.textMetadata,
+                },
+                {
+                  role: "user",
+                  content: [
+                    {
+                      type: "image",
+                      image: new URL(input.imageUrl),
+                    },
+                  ],
+                },
+              ],
+              schema: EventMetadataSchema,
+            },
+            { name: "eventFromImage.metadata" },
+          ),
+        ]);
+
+        const eventObject = { ...event.object, eventMetadata: metadata.object };
+
+        const events = addCommonAddToCalendarProps([eventObject]);
+
         // const response = `${event.rawResponse?.toString() || ""} ${metadata.rawResponse?.toString()}`;
         // return { events, response };
 
