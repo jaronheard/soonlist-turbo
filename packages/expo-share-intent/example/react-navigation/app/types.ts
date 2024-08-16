e2cbe5a77b1a580d1cda646d10e537bb5201600a
Jaron Heard <jaronheard@gmail.com>
2024-08-16 14:57:32 -0700

    remove examples

--- packages/expo-share-intent/example/react-navigation/app/types.ts
+++
@@ -1,6 +0,0 @@
-import { NavigatorScreenParams } from "@react-navigation/native";
-
-export type RootStackParamList = {
-  Home: NavigatorScreenParams<undefined>;
-  ShareIntent: NavigatorScreenParams<undefined>;
-};
