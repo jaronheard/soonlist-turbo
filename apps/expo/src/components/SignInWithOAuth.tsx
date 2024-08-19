f0085d0af78262bd7e0361d2c26926650fd537c5
Jaron Heard <jaronheard@gmail.com>
2024-08-18 22:43:46 -0700

    add signin with apple

--- apps/expo/src/components/SignInWithOAuth.tsx
+++ apps/expo/src/components/SignInWithOAuth.tsx
@@ -1,53 +1,82 @@
+import type { OAuthStrategy } from "@clerk/types";
 import React from "react";
-import { StyleSheet, View } from "react-native";
+import { View } from "react-native";
 import { Stack } from "expo-router";
 import * as WebBrowser from "expo-web-browser";
-import { useOAuth } from "@clerk/clerk-expo";
+import { useOAuth, useSignIn, useSignUp } from "@clerk/clerk-expo";
 
 import { useWarmUpBrowser } from "../hooks/useWarmUpBrowser";
+import { AppleSignInButton } from "./AppleSignInButton";
 import { GoogleSignInButton } from "./GoogleSignInButton";
 
 WebBrowser.maybeCompleteAuthSession();
 
 const SignInWithOAuth = () => {
-  // Warm up the android browser to improve UX
-  // https://docs.expo.dev/guides/authentication/#improving-user-experience
   useWarmUpBrowser();
 
-  const { startOAuthFlow } = useOAuth({ strategy: "oauth_google" });
+  const { signIn, setActive: setActiveSignIn } = useSignIn();
+  const { signUp, setActive: setActiveSignUp } = useSignUp();
+  const { startOAuthFlow: startGoogleOAuthFlow } = useOAuth({
+    strategy: "oauth_google",
+  });
+  const { startOAuthFlow: startAppleOAuthFlow } = useOAuth({
+    strategy: "oauth_apple",
+  });
 
-  const onPress = React.useCallback(async () => {
+  if (!signIn || !signUp) return null;
+
+  const handleOAuthFlow = async (strategy: OAuthStrategy) => {
     try {
-      const { createdSessionId, setActive } = await startOAuthFlow();
+      const startOAuthFlow =
+        strategy === "oauth_google"
+          ? startGoogleOAuthFlow
+          : startAppleOAuthFlow;
 
-      if (createdSessionId && setActive) {
-        void setActive({ session: createdSessionId });
-      } else {
-        // Use signIn or signUp for next steps such as MFA
+      // If the user has an account but needs to sign in
+      const userExistsButNeedsToSignIn =
+        signUp.verifications.externalAccount.status === "transferable" &&
+        signUp.verifications.externalAccount.error?.code ===
+          "external_account_exists";
+
+      if (userExistsButNeedsToSignIn) {
+        const res = await signIn.create({ transfer: true });
+        if (res.status === "complete" && setActiveSignIn) {
+          await setActiveSignIn({ session: res.createdSessionId });
+        }
+        return;
+      }
+
+      // If the user needs to be created
+      const userNeedsToBeCreated =
+        signIn.firstFactorVerification.status === "transferable";
+
+      if (userNeedsToBeCreated) {
+        const res = await signUp.create({ transfer: true });
+        if (res.status === "complete" && setActiveSignUp) {
+          await setActiveSignUp({ session: res.createdSessionId });
+        }
+        return;
+      }
+
+      // Normal sign-in flow
+      const result = await startOAuthFlow();
+      if (result.createdSessionId && setActiveSignIn) {
+        await setActiveSignIn({ session: result.createdSessionId });
       }
     } catch (err) {
       console.error("OAuth error", err);
     }
-  }, [startOAuthFlow]);
+  };
 
   return (
-    <View style={styles.container}>
-      <Stack.Screen
-        options={{
-          title: "Soonlist",
-        }}
+    <View className="flex-1 items-center justify-center gap-2">
+      <Stack.Screen options={{ title: "Soonlist" }} />
+      <GoogleSignInButton
+        onPress={() => void handleOAuthFlow("oauth_google")}
       />
-      <GoogleSignInButton onPress={onPress} />
+      <AppleSignInButton onPress={() => void handleOAuthFlow("oauth_apple")} />
     </View>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-  },
-});
 
 export default SignInWithOAuth;
