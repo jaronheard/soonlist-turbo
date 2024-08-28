551ab1903ea736766721f6f901fa460681ff28c6
Jaron Heard <jaronheard@gmail.com>
2024-08-27 21:51:08 -0700

    update app build number

--- apps/expo/app.config.ts
+++ apps/expo/app.config.ts
@@ -5,7 +5,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   name: "Soonlist",
   slug: "timetimecc",
   scheme: "soonlist",
-  version: "1.0.0",
+  version: "1.0.1",
   orientation: "portrait",
   icon: "./assets/icon.png",
   userInterfaceStyle: "light",
