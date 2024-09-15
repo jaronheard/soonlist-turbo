b98a0ccae27ec20edd585b38a09d58fd0ce9930a
Jaron Heard <jaronheard@gmail.com>
2024-09-15 14:45:43 -0700

    more zustand

--- apps/expo/src/store.ts
+++ apps/expo/src/store.ts
@@ -12,6 +12,23 @@ interface AppState {
   setIsCalendarModalVisible: (isVisible: boolean) => void;
   setShowAllCalendars: (show: boolean) => void;
   resetIntentParams: () => void;
+  input: string;
+  imagePreview: string | null;
+  linkPreview: string | null;
+  isCreating: boolean;
+  isPublic: boolean;
+  isImageLoading: boolean;
+  isImageUploading: boolean;
+  uploadedImageUrl: string | null;
+  setInput: (input: string) => void;
+  setImagePreview: (preview: string | null) => void;
+  setLinkPreview: (preview: string | null) => void;
+  setIsCreating: (isCreating: boolean) => void;
+  setIsPublic: (isPublic: boolean) => void;
+  setIsImageLoading: (isLoading: boolean) => void;
+  setIsImageUploading: (isUploading: boolean) => void;
+  setUploadedImageUrl: (url: string | null) => void;
+  resetAddEventState: () => void;
 }
 
 export const useAppStore = create<AppState>((set) => ({
@@ -25,4 +42,31 @@ export const useAppStore = create<AppState>((set) => ({
     set({ isCalendarModalVisible: isVisible }),
   setShowAllCalendars: (show) => set({ showAllCalendars: show }),
   resetIntentParams: () => set({ intentParams: null }),
+  input: "",
+  imagePreview: null,
+  linkPreview: null,
+  isCreating: false,
+  isPublic: false,
+  isImageLoading: false,
+  isImageUploading: false,
+  uploadedImageUrl: null,
+  setInput: (input) => set({ input }),
+  setImagePreview: (preview) => set({ imagePreview: preview }),
+  setLinkPreview: (preview) => set({ linkPreview: preview }),
+  setIsCreating: (isCreating) => set({ isCreating }),
+  setIsPublic: (isPublic) => set({ isPublic }),
+  setIsImageLoading: (isLoading) => set({ isImageLoading: isLoading }),
+  setIsImageUploading: (isUploading) => set({ isImageUploading: isUploading }),
+  setUploadedImageUrl: (url) => set({ uploadedImageUrl: url }),
+  resetAddEventState: () =>
+    set({
+      input: "",
+      imagePreview: null,
+      linkPreview: null,
+      isCreating: false,
+      isPublic: false,
+      isImageLoading: false,
+      isImageUploading: false,
+      uploadedImageUrl: null,
+    }),
 }));
