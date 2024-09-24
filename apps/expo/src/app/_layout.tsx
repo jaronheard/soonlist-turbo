0ed64e0a77847eeb2bd63f324083bb3ac10b239e
Jaron Heard <jaronheard@gmail.com>
2024-09-23 17:10:58 -0700

    setup posthog session recordings

--- apps/expo/src/app/_layout.tsx
+++ apps/expo/src/app/_layout.tsx
@@ -109,6 +109,26 @@ function RootLayout() {
                       apiKey={Config.posthogApiKey}
                       options={{
                         host: "https://us.i.posthog.com",
+                        enableSessionReplay: true,
+                        sessionReplayConfig: {
+                          // Whether text inputs are masked. Default is true.
+                          // Password inputs are always masked regardless
+                          maskAllTextInputs: false,
+                          // Whether images are masked. Default is true.
+                          maskAllImages: false,
+                          // Capture logs automatically. Default is true.
+                          // Android only (Native Logcat only)
+                          captureLog: false,
+                          // Whether network requests are captured in recordings. Default is true
+                          // Only metric-like data like speed, size, and response code are captured.
+                          // No data is captured from the request or response body.
+                          // iOS only
+                          captureNetworkTelemetry: true,
+                          // Deboucer delay used to reduce the number of snapshots captured and reduce performance impact. Default is 500ms
+                          androidDebouncerDelayMs: 500,
+                          // Deboucer delay used to reduce the number of snapshots captured and reduce performance impact. Default is 1000ms
+                          iOSdebouncerDelayMs: 1000,
+                        },
                       }}
                     >
                       <RootLayoutContent />
