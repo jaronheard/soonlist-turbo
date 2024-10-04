fbe151827435c354c3ec6808ab0ce5c2c5e3e8e7
Jaron Heard <jaronheard@gmail.com>
2024-10-04 01:55:36 -0700

    feat: Update pricing description and badges

---
+++ apps/web/app/(base)/join/page.tsx
@@ -0,0 +1,12 @@
+import PlansPage from "../account/plans/page";
+
+export const metadata = {
+  title: "Become a Founding Member | Soonlist",
+  openGraph: {
+    title: "Become a Founding Member | Soonlist",
+  },
+};
+
+export default function Page() {
+  return <PlansPage />;
+}
