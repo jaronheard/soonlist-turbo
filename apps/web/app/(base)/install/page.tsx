ab690fd9c0371599aa57e3773f8e7f43055d4f03
Jaron Heard <jaronheard@gmail.com>
2024-11-01 20:39:40 -0700

    fix(install/page.tsx): remove unused import and commented code related to stripe session verification

--- apps/web/app/(base)/install/page.tsx
+++ apps/web/app/(base)/install/page.tsx
@@ -1,9 +1,9 @@
 import { redirect } from "next/navigation";
 
-import { api } from "~/trpc/server";
+// import { api } from "~/trpc/server";
 import { TestFlightInstall } from "../get-started/GetStartedClient";
 
-export default async function InstallTestFlightPage({
+export default function InstallTestFlightPage({
   searchParams,
 }: {
   searchParams: { session_id: string };
@@ -15,13 +15,14 @@ export default async function InstallTestFlightPage({
   }
 
   // Verify the session and get the session data
-  const { success } = await api.stripe.verifyCheckoutSession({
-    sessionId,
-  });
+  // TODO: Uncomment this once we have a way to verify the session
+  // const { success } = await api.stripe.verifyCheckoutSession({
+  //   sessionId,
+  // });
 
-  if (!success) {
-    redirect("/");
-  }
+  // if (!success) {
+  //   redirect("/");
+  // }
 
   return <TestFlightInstall title="Get started with the app (iOS)" />;
 }
