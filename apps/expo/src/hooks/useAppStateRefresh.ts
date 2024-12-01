812dfa0fdee352c25072190bed193f22dde32be3
Jaron Heard <jaronheard@gmail.com>
2024-11-30 18:49:00 -0800

    fix(feed.tsx): change isLoading to isPending in eventsQuery to reflect the loading state more accurately
    * fix(feed.tsx): change isLoading to isPending in eventsQuery to reflect the loading state more accurately
    
    * fix(useAppStateRefresh): remove unused imports and refactor logic to improve performance and maintainability

--- apps/expo/src/hooks/useAppStateRefresh.ts
+++ apps/expo/src/hooks/useAppStateRefresh.ts
@@ -1,39 +1,19 @@
-import { useCallback, useEffect, useRef } from "react";
-import { AppState } from "react-native";
-
-import { api } from "~/utils/api";
+import { useEffect } from "react";
+import { AppState, Platform } from "react-native";
+import { focusManager } from "@tanstack/react-query";
 
 export function useAppStateRefresh() {
-  const utils = api.useUtils();
-  const appState = useRef(AppState.currentState);
-  const lastRefreshTime = useRef<number | null>(null);
-
-  const refreshEvents = useCallback(() => {
-    const now = Date.now();
-    if (
-      !lastRefreshTime.current ||
-      now - lastRefreshTime.current > 1000 * 60 * 15
-    ) {
-      void utils.event.invalidate();
-      lastRefreshTime.current = now;
-    }
-  }, [utils]);
-
   useEffect(() => {
-    const subscription = AppState.addEventListener("change", (nextAppState) => {
-      if (
-        appState.current.match(/inactive|background/) &&
-        nextAppState === "active"
-      ) {
-        refreshEvents();
+    function onAppStateChange(status: string) {
+      if (Platform.OS !== "web") {
+        focusManager.setFocused(status === "active");
       }
-      appState.current = nextAppState;
-    });
+    }
+
+    const subscription = AppState.addEventListener("change", onAppStateChange);
 
     return () => {
       subscription.remove();
     };
-  }, [refreshEvents, utils]);
-
-  return refreshEvents;
+  }, []);
 }
