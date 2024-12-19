f6bdf84cedd59bca0ee511e48823ece5c42ea12c
Josh Carr <joshcarr@gmail.com>
2024-12-19 14:19:05 -0800

    better expreience fo rno users

--- apps/web/app/(base)/2024/[userName]/page.tsx
+++ apps/web/app/(base)/2024/[userName]/page.tsx
@@ -1,9 +1,8 @@
 import React from "react";
-import Image from "next/image";
-// use these to check if the user is signed in
-import { SignedIn, SignedOut, SignInButton } from "@clerk/nextjs";
 
-import Section from "../_components/section";
+// use these to check if the user is signed in
+// import { SignedIn, SignedOut, SignInButton } from "@clerk/nextjs";
+
 import UserStatsCard from "../_components/userStatsCard";
 import dataForUsersFor2024 from "./dataForUsersFor2024";
 
@@ -21,11 +20,23 @@ interface Props {
 const Page = ({ params }: Props) => {
   const userName = params.userName;
 
+  let component = <div />;
   if (!userName || !dataForUsersFor2024[userName]) {
-    return <div>No data for this user</div>;
+    component = (
+      <div>
+        <p className="text-center text-xl leading-7.5 text-gray-700 md:text-2xl md:leading-9">
+          Become a member of Soonlist to join our community and start capturing
+          your events!
+        </p>
+        <p className="text-center text-xl leading-7.5 text-gray-700 md:text-2xl md:leading-9">
+          Scroll down to join now!
+        </p>
+      </div>
+    );
+  } else {
+    const userData = dataForUsersFor2024[userName];
+    component = <UserStatsCard {...userData} />;
   }
-
-  const userData = dataForUsersFor2024[userName];
 
   //   const userData =   {
   //   user_id: "user_2ZFNoiajf80Q4ZiTQj8hAgatRCt",
@@ -43,7 +54,33 @@ const Page = ({ params }: Props) => {
   //   first_event_date: "2024-01-13 06:00:00",
   // },
 
-  return <UserStatsCard {...userData} />;
+  return (
+    <div className="flex flex-col items-center justify-center">
+      <h1 className="text-center font-heading text-5xl font-bold leading-none tracking-tighterish text-gray-700 md:text-7xl md:leading-none">
+        2024 <br />
+        <span className="relative inline-block text-6xl text-interactive-1 md:text-8xl">
+          <svg
+            width="492"
+            height="96"
+            viewBox="0 0 492 96"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+            className="tranform absolute inset-0 z-[-1] h-full w-full scale-110 opacity-100"
+          >
+            <path
+              d="M0.977745 90.0631L13.3028 15.2256C13.6677 13.01 15.557 11.3673 17.8018 11.314L487.107 0.163765C490.41 0.0852941 492.749 3.36593 491.598 6.46257L474.712 51.884C474.083 53.5754 472.537 54.7535 470.739 54.9104L5.99405 95.4768C2.9558 95.742 0.482147 93.0724 0.977745 90.0631Z"
+              fill="#FEEA9F"
+            />
+          </svg>
+          captured!
+        </span>
+      </h1>
+      <p className="mt-6 text-xl leading-7.5 text-gray-700 md:text-2xl md:leading-9">
+        Here's how you did in 2024!
+      </p>
+      {component}
+    </div>
+  );
 };
 
 export default Page;
