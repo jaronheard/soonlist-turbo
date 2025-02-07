694f063794b17373b183f77207b5cb5ef8a5e8d8
Jaron Heard <jaronheard@gmail.com>
2025-02-07 10:16:21 -0800

    📝 update privacy policy with comprehensive iOS app data collection details

--- apps/web/app/(marketing)/privacy/page.tsx
+++ apps/web/app/(marketing)/privacy/page.tsx
@@ -10,66 +10,171 @@ export default function Page() {
     <div className="bg-white px-6 py-32 lg:px-8">
       <div className="prose mx-auto sm:prose-lg lg:prose-xl xl:prose-2xl 2xl:prose-2xl">
         <h1>Privacy Policy for Soonlist</h1>
+        <p>Last updated: February 7, 2025</p>
         <p>
-          At Soonlist, accessible from Soonlist.com, one of our main priorities
-          is the privacy of our visitors. This Privacy Policy document contains
-          types of information that is collected and recorded by Soonlist and
-          how we use it.
+          At Soonlist ("we," "us," or "our"), accessible from
+          https://www.soonlist.com, one of our main priorities is the privacy of
+          our visitors and users. This Privacy Policy document contains details
+          about the types of information that Soonlist collects and records via
+          our website and our iOS application ("App"), as well as how we use and
+          protect that information. By using our website or iOS App, you hereby
+          consent to this Privacy Policy and agree to the terms herein.
         </p>
         <p>
           If you have additional questions or require more information about our
           Privacy Policy, do not hesitate to contact us.
         </p>
-        <h2>Log Files</h2>
+        <h2>1. Scope</h2>
         <p>
-          Soonlist follows a standard procedure of using log files. These files
-          log visitors when they visit websites. All hosting companies do this
-          and a part of hosting services' analytics. The information collected
-          by log files include internet protocol (IP) addresses, browser type,
-          Internet Service Provider (ISP), date and time stamp, referring/exit
-          pages, and possibly the number of clicks. These are not linked to any
-          information that is personally identifiable. The purpose of the
-          information is for analyzing trends, administering the site, tracking
-          users' movement on the website, and gathering demographic information.
-          Our Privacy Policy was created with the help of the{" "}
-          <a href="https://www.privacypolicygenerator.org">
-            Privacy Policy Generator
-          </a>
-          .
+          This Privacy Policy applies to information collected online through
+          the Soonlist website and through our iOS App. It does not apply to any
+          information collected offline or via channels other than this website
+          or our iOS App.
         </p>
-        <h2>Privacy Policies</h2>
+        <h2>2. Information We Collect and How We Use It</h2>
+        <h3>A. Website Log Files</h3>
         <p>
-          You may consult this list to find the Privacy Policy for each of the
-          advertising partners of Soonlist.
+          Soonlist follows a standard procedure of using log files for the
+          website. These files log visitors when they visit websites. All
+          hosting companies do this as part of hosting services' analytics. The
+          information collected by log files includes internet protocol (IP)
+          addresses, browser type, Internet Service Provider (ISP), date and
+          time stamp, referring/exit pages, and possibly the number of clicks.
+          These are not linked to any information that is personally
+          identifiable. The purpose of the information is to analyze trends,
+          administer the site, track users' movement on the website, and gather
+          demographic information.
         </p>
+        <h3>B. iOS App Data Collection</h3>
         <p>
-          Third-party ad servers or ad networks uses technologies like cookies,
-          JavaScript, or Web Beacons that are used in their respective
-          advertisements and links that appear on Soonlist, which are sent
-          directly to users' browser. They automatically receive your IP address
-          when this occurs. These technologies are used to measure the
-          effectiveness of their advertising campaigns and/or to personalize the
-          advertising content that you see on websites that you visit.
+          When you use our iOS App, we may collect certain data to provide and
+          improve our services, ensure the App's functionality, and personalize
+          your experience. This data may be linked to your identity, as
+          permitted under Apple's guidelines. The data types we collect include:
         </p>
+        <ol>
+          <li>
+            Contact Info
+            <ul>
+              <li>
+                Name (for product personalization, analytics, app functionality,
+                and our marketing)
+              </li>
+              <li>
+                Email Address (for analytics, personalization, app
+                functionality, and marketing)
+              </li>
+              <li>
+                Phone Number (for marketing, app functionality, personalization,
+                and analytics)
+              </li>
+              <li>
+                Other User Contact Info (for marketing, analytics, app
+                functionality, and personalization)
+              </li>
+            </ul>
+          </li>
+          <li>
+            User Content
+            <ul>
+              <li>
+                Photos or Videos that you choose to share or upload (for app
+                functionality, personalization, and analytics)
+              </li>
+              <li>
+                Customer Support messages or attachments (for handling support
+                requests, personalization, and analytics)
+              </li>
+            </ul>
+          </li>
+          <li>
+            Identifiers
+            <ul>
+              <li>
+                User ID (for personalization, marketing, app functionality, and
+                analytics)
+              </li>
+              <li>Device ID (for analytics and app functionality)</li>
+            </ul>
+          </li>
+          <li>
+            Usage Data
+            <ul>
+              <li>
+                Product Interaction (for analytics, app functionality,
+                marketing, and personalization)
+              </li>
+            </ul>
+          </li>
+          <li>
+            Diagnostics
+            <ul>
+              <li>
+                Crash Data (for analytics, personalization, and app
+                functionality)
+              </li>
+              <li>
+                Performance Data (for app functionality, personalization, and
+                analytics)
+              </li>
+              <li>
+                Other Diagnostic Data (for personalization, analytics, and app
+                functionality)
+              </li>
+            </ul>
+          </li>
+        </ol>
         <p>
-          Note that Soonlist has no access to or control over these cookies that
-          are used by third-party advertisers.
+          We may combine the data we collect (e.g., contact details and
+          identifiers) to improve our services and tailor them to you. We also
+          use this information to monitor for performance issues, detect fraud,
+          and perform internal analytics.
         </p>
-        <h2>Third Party Privacy Policies</h2>
+        <h2>3. How We Share Your Information</h2>
+        <ol>
+          <li>
+            Third-Party Service Providers
+            <p>
+              We may share information with service providers (e.g., analytics
+              providers, hosting services) who assist us in operating our
+              website or iOS App, conducting our business, or servicing you.
+            </p>
+          </li>
+          <li>
+            Advertising Partners
+            <p>
+              Third-party ad servers or ad networks may use technologies like
+              cookies, JavaScript, or Web Beacons in advertisements and links
+              that appear on Soonlist. They automatically receive your IP
+              address when this occurs. These technologies measure the
+              effectiveness of advertising campaigns or personalize the
+              advertising content you see. Soonlist has no access to or control
+              over these cookies that are used by third-party advertisers.
+            </p>
+          </li>
+          <li>
+            Legal Compliance and Protection
+            <p>
+              We may disclose your information if required to do so by law,
+              subpoena, or similar legal process, or if we believe that such
+              action is necessary to comply with legal obligations or protect
+              and defend our rights, property, or the personal safety of our
+              users or the public.
+            </p>
+          </li>
+        </ol>
+        <h2>4. Third Party Privacy Policies</h2>
         <p>
-          Soonlist's Privacy Policy does not apply to other advertisers or
-          websites. Thus, we are advising you to consult the respective Privacy
-          Policies of these third-party ad servers for more detailed
-          information. It may include their practices and instructions about how
-          to opt-out of certain options.{" "}
+          Soonlist's Privacy Policy does not apply to other advertisers,
+          websites, or apps. We advise you to consult the respective privacy
+          policies of any third-party services for more detailed information on
+          their practices, including instructions on how to opt out of certain
+          options. You can choose to disable cookies through your individual
+          browser options. More detailed information about cookie management for
+          specific web browsers can be found on the browsers' respective
+          websites.
         </p>
-        <p>
-          You can choose to disable cookies through your individual browser
-          options. To know more detailed information about cookie management
-          with specific web browsers, it can be found at the browsers'
-          respective websites.
-        </p>
-        <h2>Google OAuth</h2>
+        <h2>5. Google OAuth</h2>
         <p>
           Soonlist's use and transfer to any other app of information received
           from Google APIs will adhere to the{" "}
@@ -78,31 +183,74 @@ export default function Page() {
           </a>
           , including the Limited Use requirements.
         </p>
-        <h2>Children's Information</h2>
+        <h2>6. Children's Information</h2>
         <p>
-          Another part of our priority is adding protection for children while
-          using the internet. We encourage parents and guardians to observe,
-          participate in, and/or monitor and guide their online activity.
-        </p>
-        <p>
+          Protecting children using the internet is very important to us.
           Soonlist does not knowingly collect any Personal Identifiable
-          Information from children under the age of 13. If you think that your
-          child provided this kind of information on our website, we strongly
-          encourage you to contact us immediately and we will do our best
-          efforts to promptly remove such information from our records.
+          Information from children under the age of 13. If you believe that
+          your child provided this information on our website or via our iOS
+          App, please contact us immediately, and we will do our best to remove
+          such information from our records promptly.
         </p>
-        <h2>Online Privacy Policy Only</h2>
+        <h2>7. Data Security</h2>
+        <p>
+          We take reasonable steps to protect the information we collect against
+          loss, misuse, and unauthorized access, disclosure, alteration, or
+          destruction. However, please note that no electronic transmission or
+          storage of data can be guaranteed to be 100% secure. You use the
+          website and iOS App at your own risk.
+        </p>
+        <h2>8. Your Choices and Control</h2>
+        <ul>
+          <li>
+            Opt-Out of Marketing Communications: You can opt out of receiving
+            marketing or promotional emails from us by following the unsubscribe
+            link in those emails or contacting us directly.
+          </li>
+          <li>
+            App Permissions: You may control the iOS App's access to various
+            device features (e.g., camera, photos) through your device's
+            settings.
+          </li>
+          <li>
+            Cookies: As noted, you can choose to disable cookies through your
+            browser settings.
+          </li>
+        </ul>
+        <h2>9. Online Privacy Policy Only</h2>
         <p>
           This Privacy Policy applies only to our online activities and is valid
-          for visitors to our website with regards to the information that they
-          shared and/or collect in Soonlist. This policy is not applicable to
+          for visitors to our website and iOS App regarding the information that
+          they share and/or collect in Soonlist. This policy does not apply to
           any information collected offline or via channels other than this
-          website.
+          website or our iOS App.
+        </p>
+        <h2>10. Changes to this Policy</h2>
+        <p>
+          We may update our Privacy Policy from time to time. Any changes will
+          be posted on this page, with an updated "Last updated" date. We
+          encourage you to review this Privacy Policy periodically for any
+          updates. Your continued use of the website or iOS App after any
+          changes signifies your acceptance of the revised policy.
+        </p>
+        <h2>11. Contact Us</h2>
+        <p>
+          If you have any questions, comments, or concerns about this Privacy
+          Policy or our practices, please contact us:
+        </p>
+        <p>
+          Soonlist
+          <br />
+          2261 Market Street, STE 5997
+          <br />
+          San Francisco, CA 94114, United States
+          <br />
+          Email: jaron@soonlist.com
         </p>
         <h2>Consent</h2>
         <p>
-          By using our website, you hereby consent to our Privacy Policy and
-          agree to its Terms and Conditions.
+          By using our website or iOS App, you hereby consent to our Privacy
+          Policy and agree to its terms and conditions.
         </p>
       </div>
     </div>
