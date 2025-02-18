be8f4d1a53e7454f95d3eb9f39a8037ee0743871
Jaron Heard <jaronheard@gmail.com>
2025-02-18 14:14:22 -0800

    1.0.16

--- apps/expo/app.config.ts
+++ apps/expo/app.config.ts
@@ -45,7 +45,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   name: getAppName(),
   slug: "timetimecc",
   scheme: getScheme(),
-  version: "1.0.15",
+  version: "1.0.16",
   orientation: "portrait",
   icon: IS_DEV ? "./assets/icon-dev.png" : "./assets/icon.png",
   userInterfaceStyle: "light",
