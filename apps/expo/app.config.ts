007e647c963793180e15ac9a2e866673d2915ab2
Jaron Heard <jaronheard@gmail.com>
2025-02-21 01:12:19 -0800

    Revert "🔧 bump app version from 1.0.19 to 1.0.20"
    This reverts commit e1160cd16d8507b83e7fa3d6c9be89b9d3c16a83.

--- apps/expo/app.config.ts
+++ apps/expo/app.config.ts
@@ -45,7 +45,7 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   name: getAppName(),
   slug: "timetimecc",
   scheme: getScheme(),
-  version: "1.0.20",
+  version: "1.0.19",
   orientation: "portrait",
   icon: IS_DEV ? "./assets/icon-dev.png" : "./assets/icon.png",
   userInterfaceStyle: "light",
