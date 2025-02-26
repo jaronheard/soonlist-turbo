33b5a08759eecc251c21099264a969e9bbc72c7e
Jaron Heard <jaronheard@gmail.com>
2025-02-26 08:36:25 -0800

    sync target configs

--- apps/expo/targets/live-activity/expo-target.config.js
+++ apps/expo/targets/live-activity/expo-target.config.js
@@ -3,4 +3,13 @@ module.exports = {
   type: "live-activity",
   name: "LiveActivity",
   frameworks: ["SwiftUI", "ActivityKit"],
+  icon: "../../assets/icon.png",
+  deploymentTarget: "15.1",
+  entitlements: {
+    "com.apple.security.application-groups": [
+      process.env.APP_VARIANT === "development"
+        ? "group.com.soonlist.dev"
+        : "group.com.soonlist",
+    ],
+  },
 };
