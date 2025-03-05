e436c19c5001578f23004a5567b350b56fe25d9c
Jaron Heard <jaronheard@gmail.com>
2025-03-04 17:49:55 -0800

    Update apps/expo/src/app/settings/account.tsx

--- apps/expo/src/app/settings/account.tsx
+++ apps/expo/src/app/settings/account.tsx
@@ -546,7 +546,7 @@ export default function EditProfileScreen() {
                       className="mt-2 rounded-md bg-neutral-100 p-4"
                     >
                       <Text className="text-base">
-                        View subscription in Settings
+                        View subscription in  Settings
                       </Text>
                     </TouchableOpacity>
                   );
